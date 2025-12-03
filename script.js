57df34efb513dd005f82444c49e51c505030970b
tess-shirley <119884843+tessshirley@users.noreply.github.com>
2025-12-03 21:37:35 +0000

    finalizing

--- script.js
+++ script.js
@@ -185,8 +185,7 @@ class PhysicsMelodyBuilder {
     getScalePatterns() {
         return {
             'major': [0, 2, 4, 5, 7, 9, 11],
-            'minor': [0, 2, 3, 5, 7, 8, 10],
-            'pentatonic': [0, 2, 4, 7, 9]
+            'minor': [0, 2, 3, 5, 7, 8, 10]
         };
     }
 
